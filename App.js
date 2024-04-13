3b0e25d7ae295bbeba89360a2aee7be6e7407302
LASmelov <smelov020@gmail.com>
2024-04-13 09:45:40 +0300

    add counter

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image, TouchableHighlight } from 'react-native';
 
 export default function App() {
   const [notification, setNotification] = useState('Начинайте')
@@ -14,15 +14,23 @@ export default function App() {
     " ", " ", " ",
     " ", " ", " "
   ])
+
+
+  const [xWins, setXWins] = useState(0);
+  const [oWins, setOWins] = useState(0);
+
+
+
+
   const pressField = (index) => {
     let newBoard = board
-    if(newBoard[index]!== "X" && newBoard[index]!== "O"){
+    if (newBoard[index] !== "X" && newBoard[index] !== "O") {
       if (currentPlayer == "X") {
         newBoard[index] = "X"
         setCurrentPlayer("O")
 
         setNotification("Очередь нолика")
-      }else{
+      } else {
         newBoard[index] = "O"
         setCurrentPlayer("X")
 
@@ -34,70 +42,76 @@ export default function App() {
 
       checkIdPlayerWin()
     }
- 
-};
 
-const checkIdPlayerWin = () =>{
+  };
 
-if (board[0] == board[1] && board[1] == board[2] && board[0] !== " "){
-  playerWin(board[0])
-}
-else if(board[3] == board[4] && board[4] == board[5] &&  board[5] !== " "){
-  playerWin(board[3])
-}
-else if(board[6] == board[7] && board[7] == board[8] &&  board[8] !== " "){
-  playerWin(board[6])
-}
-else if(board[0] == board[4] && board[4] == board[8] &&  board[8] !== " "){
-  playerWin(board[0])
-}
-else if(board[0] == board[3] && board[3] == board[6] &&  board[6] !== " "){
-  playerWin(board[0])
-}
-else if(board[1] == board[4] && board[4] == board[7] &&  board[7] !== " "){
-  playerWin(board[1])
-}
-else if(board[2] == board[4] && board[4] == board[6] &&  board[6] !== " "){
-  playerWin(board[2])
-}
-else if(board[2] == board[5] && board[5] == board[8] &&  board[8] !== " "){
-  playerWin(board[2])
-}
-};
-const playerWin = (textWin) => {
-  alert("Игрок " + textWin + " выиграл")
-  setBoard([
-    " ", " ", " ",
-    " ", " ", " ",
-    " ", " ", " "
-  ])
-  if(textWin =="O"){
-    setNotification("X начинает")
-  }else{
-    setNotification("O начинает")
+  const checkIdPlayerWin = () => {
+
+    if (board[0] == board[1] && board[1] == board[2] && board[0] !== " ") {
+      playerWin(board[0])
+    }
+    else if (board[3] == board[4] && board[4] == board[5] && board[5] !== " ") {
+      playerWin(board[3])
+    }
+    else if (board[6] == board[7] && board[7] == board[8] && board[8] !== " ") {
+      playerWin(board[6])
+    }
+    else if (board[0] == board[4] && board[4] == board[8] && board[8] !== " ") {
+      playerWin(board[0])
+    }
+    else if (board[0] == board[3] && board[3] == board[6] && board[6] !== " ") {
+      playerWin(board[0])
+    }
+    else if (board[1] == board[4] && board[4] == board[7] && board[7] !== " ") {
+      playerWin(board[1])
+    }
+    else if (board[2] == board[4] && board[4] == board[6] && board[6] !== " ") {
+      playerWin(board[2])
+    }
+    else if (board[2] == board[5] && board[5] == board[8] && board[8] !== " ") {
+      playerWin(board[2])
+    }
+  };
+  const playerWin = (textWin) => {
+    alert("Игрок " + textWin + " выиграл")
+    setBoard([
+      " ", " ", " ",
+      " ", " ", " ",
+      " ", " ", " "
+    ])
+    if (textWin == "O") {
+      setNotification("X начинает")
+      setOWins(oWins + 1);
+    } else {
+      setNotification("O начинает")
+      setXWins(xWins + 1);
+    }
   }
-}
 
   return (
     <View style={styles.container}>
-        <Image source={require('./assets/bg.jpg')} style={styles.imagebg}/>
+      <Image source={require('./assets/bg.jpg')} style={styles.imagebg} />
       <View style={styles.namegame}>Крестики нолики</View>
       <Text style={styles.text}>{notification}</Text>
 
+      <View style={styles.WinСounter}>
+        <Text style={styles.text}>Побед X: {xWins}</Text>
+        <Text style={styles.text}>Побед O: {oWins}</Text>
+      </View>
 
-<View>
-  <Image source={require('./assets/R (3).png')} style={styles.image2}/>
+      <View>
+        <Image source={require('./assets/R (3).png')} style={styles.image2} />
 
 
-      <FlatList style={styles.list} data={board} extraData={refresh} numColumns={3} renderItem={({ item, index }) => (
+        <FlatList style={styles.list} data={board} extraData={refresh} numColumns={3} renderItem={({ item, index }) => (
 
-        <TouchableOpacity style={styles.square} onPress={() => pressField(index)}>
-          <Text style={styles.item}>{item}</Text>
-        </TouchableOpacity>
-      )}
-      />
+          <TouchableOpacity style={styles.square} onPress={() => pressField(index)}>
+            <Text style={styles.item}>{item}</Text>
+          </TouchableOpacity>
+        )}
+        />
 
-      <StatusBar style="auto" /> 
+        <StatusBar style="auto" />
       </View>
     </View>
   );
@@ -125,9 +139,11 @@ const styles = StyleSheet.create({
     height: 300
   },
   square: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
     height: 60,
     width: 60,
-    backgroundColor: 'red',
     margin: 20
   },
   item: {
@@ -135,17 +151,20 @@ const styles = StyleSheet.create({
 
   },
 
-  imagebg:{
-width: "100%",
-height: '100%',
-position:'absolute',
-zIndex:-1
+  imagebg: {
+    width: "100%",
+    height: '100%',
+    position: 'absolute',
+    zIndex: -1
   },
 
-  image2:{
+  image2: {
     width: 300,
-height: 300,
-position:'absolute',
+    height: 300,
+    position: 'absolute',
+  },
+  WinСounter: {
+    flexDirection: 'row'
 
   }
 });
